79444ea4256a825bef7ba06e8ff0c91fa9ba37bc
lilound <lilounadlerr@gmail.com>
2026-02-11 11:56:56 +0100

    Update MyDsp.h

--- MyDsp.h
+++ MyDsp.h
@@ -1,17 +1,27 @@
-#ifndef my_dsp_h_
-#define my_dsp_h_
+#ifndef MYDSP_H
+#define MYDSP_H
 
+#include "Arduino.h"
 #include "AudioStream.h"
+#include "Audio.h"
+#include "Additive.h"
 #include "Peak.h"
 
 class MyDsp : public AudioStream {
 public:
-    MyDsp() : AudioStream(1, new audio_block_t*[1]) {}
-    virtual void update(void);
-    void setParameters(float gain, float freq, float bw);
+  MyDsp();
+  virtual void update(void);
+  void setFreq(float f);
+  void setFilter(float gain, float centerFreq, float bandwidth);
+  void setEar(int mode); // pour gérer sur quelle oreille on envoie : canal 0 : le gauche, canal 1 : le droit, canal 2 : les 2
+  void setMute(bool mute); // couper le son pour de vrai
 
-private:
-    PeakFilter peak;
+  private:
+
+  bool _mute = false;
+  Additive source; // Générateur de son
+  PeakFilter filter; // Notre filtre Peak EQ
+  int earMode;
 };
 
 #endif
