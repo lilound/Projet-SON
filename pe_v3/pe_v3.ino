7442a1d6d5aa2ebe675c9042ffcc27b15fea727a
lilound <lilounadlerr@gmail.com>
2026-02-11 17:10:05 +0100

    Update pe_v3.ino

--- pe_v3/pe_v3.ino
+++ pe_v3/pe_v3.ino
@@ -4,173 +4,185 @@
 MyDsp myDsp;
 AudioOutputI2S out;
 AudioControlSGTL5000 audioShield;
-
 AudioConnection patchCord1(myDsp, 0, out, 0);
 AudioConnection patchCord2(myDsp, 1, out, 1);
 
+// --- CONFIGURATION AUDIOGRAMME ---
+float frequencesStandard[] = {125, 250, 500, 1000, 2000, 4000, 8000};
+int indexFreq = 0;
+int totalFreq = 7;
+
+// --- RÉGLAGES DU DIAGNOSTIC ---
+float seuilDiagnosticdB = 40.0; // <--- MODIFIEZ CETTE VALEUR ICI (ex: 25.0 pour plus de sensibilité)
+float dbPerteHL = 0.0; 
+unsigned long tempsDebutPalier = 0;
+bool enPauseEntreFrequences = false;
+
 int buttonState = 0;
 int oldButtonState = 0;
-float freq = 125.0;
-int init_count = millis();
-bool passer = false;
-int ind = 0;
+int earMode = 0; 
 
-//Gauche ou droite ? 
-int earMode = 0 ;
-
-// surdité 
-float sourdG[20], sourdD[20];
+struct Resultat {
+  float frequence;
+  float perteDB;
+};
+Resultat sourdG[7], sourdD[7];
 int indG = 0, indD = 0;
 
-// pointeurs pour les fonctions 
-float* sourdActuel; 
-int* indActuel;
-
-// si il faut pousser le diagnostique (certaines fréquences ne sont pas entendues):
-bool enDiagnostic = false;
-int indexTrouEnCours = 0;
-float freqDepartDiag;
-
-//à comprendre plus tard 
 struct Intervalle {
   float debut;
   float fin;
 };
-
 Intervalle trousPrecisG[10], trousPrecisD[10];
 int nbTrousG = 0, nbTrousD = 0;
+
+// --- PROTOTYPES ---
+float dbToLin(float dbPerte);
+void enregistrerResultat(float f, float db);
+void preparerFrequenceSuivante();
+void passerAOreilleSuivante();
+void afficherResultats();
+void lancerDiagnosticZones(Resultat* resultats, int nbRes, int ear, Intervalle* trous, int* nbTrous);
 
 void setup() {
   pinMode(0, INPUT);
   AudioMemory(16);
   audioShield.enable();
-  audioShield.volume(0.025); 
-  // Initialisation des pointeurs sur la Gauche au départ
-  sourdActuel = sourdG;
-  indActuel = &indG;
+  audioShield.volume(0.5); 
+  
   myDsp.setEar(earMode);
+  myDsp.setMute(false);
+  tempsDebutPalier = millis();
+  Serial.begin(9600);
+  Serial.print("Diagnostic configuré pour seuil >= "); Serial.print(seuilDiagnosticdB); Serial.println(" dB HL");
 }
 
 void loop() {
-  // --- SÉCURITÉ ANTI-CRASH ---
-  if (freq > 17000.0 || freq < 20.0) { 
-    freq = 125.0;
-    if (earMode == 0){
-      earMode = 1;
-      myDsp.setEar(earMode);
-      // switch des pointeurs pour enregistrer à droite 
-      sourdActuel = sourdD;
-      indActuel = &indD;
-    }
-    else if (earMode == 1){
-      // dcp : si jamais on est arrivé à 16000 et que liste sourd non vide : on lance fonction de tests et on recup resultats pour savoir l'intervale précis de surdité
-      
+  float freqActuelle = frequencesStandard[indexFreq];
 
-      lancerDiagnostic(sourdG, indG, 0, trousPrecisG, &nbTrousG); // 0 pour Gauche
-      lancerDiagnostic(sourdD, indD, 1, trousPrecisD, &nbTrousD); // 1 pour Droite
-      myDsp.setMute(true);
+  if (!enPauseEntreFrequences) {
+    myDsp.setFreq(freqActuelle);
+    myDsp.setFilter(15.0, freqActuelle, 100.0);
+    audioShield.volume(dbToLin(dbPerteHL)); 
     
-      // Affichage des intervalles réels
-      Serial.println("\n========= RÉSULTATS PRÉCIS =========");
-      for(int i=0; i<nbTrousG; i++) {
-          Serial.print("Trou Oreille G : "); Serial.print(trousPrecisG[i].debut); 
-          Serial.print("Hz - "); Serial.print(trousPrecisG[i].fin); Serial.println("Hz");
-      }
-      for(int i=0; i<nbTrousD; i++) {
-          Serial.print("Trou Oreille D : "); Serial.print(trousPrecisD[i].debut); 
-          Serial.print("Hz - "); Serial.print(trousPrecisD[i].fin); Serial.println("Hz");
-      }
-      while(1);
-    }
-    
+    // Sortie Plotter
+    Serial.print(freqActuelle); Serial.print(" ");
+    Serial.println(-dbPerteHL); 
   }
-  myDsp.setFreq(freq);
-  // On règle aussi le filtre pour qu'il suive la fréquence (sinon on n'entend rien)
-  myDsp.setFilter(15.0, freq, 100.0);
 
-  delay(10);
-  if ((millis() - init_count) >= 6000){
-    sourdActuel[*indActuel] = freq; // On remplit le tableau pointé
-    Serial.println(sourdActuel[*indActuel]);
-    (*indActuel)++;
-    freq *= 2.0; 
-    passer = true;
-    init_count = millis(); 
-  }
   buttonState = digitalRead(0);
-  //Serial.println(buttonState);
-  if (buttonState and !oldButtonState) {
-    Serial.print("la fréquence "); Serial.print(freq); Serial.println(" est entendue.");
-    init_count = millis(); 
-    myDsp.setMute(true);
-    oldButtonState = 1;
-    passer = true;
-  }
 
-  else if (passer){
-    //Serial.print(freq); Serial.println(" pas entendue."); 
-    passer = false; 
+  if (buttonState && !oldButtonState) {
     myDsp.setMute(true);
-    delay(1000);
-    myDsp.setMute(false);
-    oldButtonState = 0;
-    freq *= 2.0;
-    audioShield.volume(0.025); 
-    
+    enregistrerResultat(freqActuelle, dbPerteHL);
+    preparerFrequenceSuivante();
+    oldButtonState = 1;
+  } 
+  else if (!buttonState) {
+    if (oldButtonState == 1) oldButtonState = 0;
+    if (millis() - tempsDebutPalier > 4000) {
+      dbPerteHL += 10.0; 
+      if (dbPerteHL > 61.0) { 
+        enregistrerResultat(freqActuelle, 70.0);
+        preparerFrequenceSuivante();
+      }
+      tempsDebutPalier = millis();
+    }
   }
-  
 }
 
-void lancerDiagnostic(float* listeSourd, int nombreSourd, int ear, Intervalle* resultats, int* nbResultats) {
+// --- LOGIQUE DE DIAGNOSTIC AVEC VARIABLE ---
+void lancerDiagnosticZones(Resultat* res, int nbRes, int ear, Intervalle* trous, int* nbTrous) {
   myDsp.setEar(ear);
-  
-  for (int i = 0; i < nombreSourd; i++) {
-    float fCentrale = listeSourd[i];
-    float fBas = fCentrale / 2.0; // On repart de la dernière fréquence entendue
-    float fHaut = fCentrale;
-    
-    Serial.print("\n--- Analyse fine autour de "); Serial.print(fCentrale); Serial.println(" Hz ---");
-    
-    float debutTrou = 0;
-    bool dansLeTrou = false;
+  for (int i = 0; i < nbRes; i++) {
+    // On utilise ici la variable globale pour le test
+    if (res[i].perteDB >= seuilDiagnosticdB) {
+      Serial.print("!!! Zone suspecte détectée à "); Serial.print(res[i].frequence); Serial.println(" Hz");
+      
+      float fBase = res[i].frequence / 1.414;
+      float fHaut = res[i].frequence * 1.414;
+      float debutTrou = 0;
+      bool dansLeTrou = false;
+      
+      // On teste à un volume constant (le seuil trouvé) pour voir la largeur du trou
+      audioShield.volume(dbToLin(res[i].perteDB)); 
 
-    // Balayage fin : on divise l'intervalle en 10 paliers
-    float pas = (fHaut - fBas) / 10.0;
-    
-    for (float f = fBas; f <= fHaut; f += pas) {
-      myDsp.setFreq(f);
-      myDsp.setFilter(20.0, f, 40.0); // Les FILTRES EN SÉRIE rendent ce pic super précis
-      
-      unsigned long startLocal = millis();
-      bool entendu = false;
-      
-      // On attend 2 secondes que l'utilisateur appuie
-      while (millis() - startLocal < 5000) {
-        if (digitalRead(0) == HIGH) {
-          entendu = true;
-          myDsp.setMute(true);
-          break;
+      for (float f = fBase; f <= fHaut; f *= 1.1) {
+        myDsp.setFreq(f);
+        myDsp.setMute(false);
+        unsigned long start = millis();
+        bool entendu = false;
+        while(millis() - start < 2000) {
+          if(digitalRead(0) == HIGH) { entendu = true; break; }
         }
+        
+        if (!entendu && !dansLeTrou) {
+          debutTrou = f; dansLeTrou = true;
+        } else if (entendu && dansLeTrou) {
+          trous[*nbTrous] = {debutTrou, f};
+          (*nbTrous)++;
+          dansLeTrou = false;
+        }
+        myDsp.setMute(true);
+        delay(200);
       }
-
-      if (!entendu && !dansLeTrou) {
-        debutTrou = f;
-        dansLeTrou = true;
-        Serial.print("Début du silence détecté à : "); Serial.println(f);
-      } 
-      else if (entendu && dansLeTrou) {
-        resultats[*nbResultats] = {debutTrou, f};
-        (*nbResultats)++;
-        dansLeTrou = false;
-        Serial.print("Fin du silence détectée à : "); Serial.println(f);
-      }
-      
-      // Petit silence entre deux tests pour ne pas saturer l'oreille
-      
-      myDsp.setMute(true);
-      delay(1000);
-      myDsp.setMute(false);
-      audioShield.volume(0.025);
     }
   }
+}
+
+// --- FONCTIONS DE GESTION (STABLES) ---
+float dbToLin(float dbPerte) {
+  float gainBase = -60.0; 
+  float dbReel = gainBase + dbPerte;
+  float amplitudeMax = 0.15; 
+  return amplitudeMax * pow(10.0, dbReel / 20.0);
+}
+
+void preparerFrequenceSuivante() {
+  myDsp.setMute(true);
+  enPauseEntreFrequences = true;
+  delay(2000); 
+  indexFreq++;
+  if (indexFreq >= totalFreq) {
+    passerAOreilleSuivante();
+  } else {
+    dbPerteHL = 0.0; 
+    enPauseEntreFrequences = false;
+    myDsp.setMute(false);
+    tempsDebutPalier = millis();
+  }
+}
+
+void passerAOreilleSuivante() {
+  if (earMode == 0) {
+    lancerDiagnosticZones(sourdD, indD, 0, trousPrecisD, &nbTrousD);
+    earMode = 1;
+    myDsp.setEar(earMode);
+    indexFreq = 0;
+    dbPerteHL = 0.0;
+    enPauseEntreFrequences = false;
+    myDsp.setMute(false);
+    tempsDebutPalier = millis();
+  } else {
+    lancerDiagnosticZones(sourdG, indG, 1, trousPrecisG, &nbTrousG);
+    afficherResultats();
+    while(1); 
+  }
+}
+
+void enregistrerResultat(float f, float db) {
+  if (earMode == 0) { sourdD[indD++] = {f, db}; } 
+  else { sourdG[indG++] = {f, db}; }
+}
+
+void afficherResultats() {
+  // On envoie une ligne de début pour que Python sache quand commencer à lire
+  Serial.println("START_DATA");
+  for(int i=0; i<totalFreq; i++) {
+    Serial.print(frequencesStandard[i]); Serial.print(",");
+    Serial.print(sourdD[i].perteDB); Serial.print(",");
+    Serial.println(sourdG[i].perteDB);
+  }
+  Serial.println("END_DATA");
+
 }
