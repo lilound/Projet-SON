c4a8cd4ac32a81320b47e9c42813776206636f82
lilound <lilounadlerr@gmail.com>
2026-02-11 11:57:45 +0100

    Update Peak.h

--- Peak.h
+++ Peak.h
@@ -6,13 +6,16 @@
 class PeakFilter {
 public:
     PeakFilter(float SR = 44100.0);
+    // Lfx: Gain (dB), fx: Fréquence (Hz), B: Bande passante (Hz)
     void setup(float Lfx, float fx, float B);
-    float tick(float input); // Traitement échantillon par échantillon
+    float tick(float input);
 
 private:
-    float b0, b1, b2, a1, a2;
-    float x1, x2, y1, y2; // États (mémoire du filtre)
     float _SR;
+    // Coefficients du filtre
+    float b0, b1, b2, a1, a2;
+    // Mémoire du filtre (les "états")
+    float x1, x2, y1, y2;
 };
 
 #endif
