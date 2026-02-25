e1625a08b453c5447e3c596216153289e688d005
lilound <lilounadlerr@gmail.com>
2026-02-25 11:11:21 +0100

    Update MyDsp.cpp with acouphenes

--- main/MyDsp.cpp
+++ main/MyDsp.cpp
@@ -1,9 +1,11 @@
 #include "MyDsp.h"
+#include "Additive.h"
 
 // Un seul constructeur propre
-MyDsp::MyDsp() : AudioStream(1, inputQueueArray), source(44100), earMode(2) {
+MyDsp::MyDsp() : AudioStream(1, inputQueueArray), source(44100), earMode(2), oscilloAcouphene(44100){
   _isDiagnostic = false;
   _mute = true; 
+  acoupheneActif = false;
   for(int i=0; i<7; i++) filters[i].setup(0.0, 1000.0, 100.0); 
 }
 
@@ -18,6 +20,27 @@ void MyDsp::setEar(int mode) { earMode = mode; }
 
 void MyDsp::setMute(bool mute) { _mute = mute; }
 void MyDsp::setDiagnostic(bool diagnostic) { _isDiagnostic = diagnostic; }
+void MyDsp::setAcouphene(bool actif, float freq = 0, float age = 20) {
+    acoupheneActif = actif;
+    if (actif) {
+        oscilloAcouphene.setFrequency(freq);
+        float nouveauGain = 0.02 + ((age - 20) * 0.002);
+        // Sécurité : on plafonne le gain à 0.15 pour ne pas abîmer les oreilles
+        if (nouveauGain > 0.15) nouveauGain = 0.15;
+        oscilloAcouphene.setGain(nouveauGain);
+    }
+}
+
+float MyDsp::tick(float input) {
+    float sortie = input; // Le son micro passé par tes filtres
+    
+    if (acoupheneActif) {
+        // On additionne le sifflement d'Additive au signal
+        sortie += oscilloAcouphene.tick();
+    }
+    
+    return sortie;
+}
 
 void MyDsp::update(void) {
   audio_block_t* inBlock = receiveReadOnly(0); 
@@ -34,34 +57,27 @@ void MyDsp::update(void) {
   for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
     float sample = 0;
     
-    // 1. On récupère la source (Générateur ou Micro)
+    // 1. Choix de la source
     if (_isDiagnostic) {
       sample = source.tick();
     } else if (inBlock) {
       sample = (float)inBlock->data[i] / 32768.0f;
     }
 
-    // 2. On initialise TOUJOURS avec la source
-    float processedSample = sample; 
+    float processedSample = sample;
 
-    // 3. Application de la logique Mute / Bypass
+    // 2. Application des filtres en cascade (Bypass si _mute est vrai)
     if (!_mute) {
-      // MODE FILTRÉ (Diagnostic actif ou Correction active)
       for(int j = 0; j < 7; j++) {
         processedSample = filters[j].tick(processedSample);
       }
-    } 
-    else if (_isDiagnostic) {
-      // MODE MUTE en Diagnostic : Silence total entre les bips
-      processedSample = 0.0f; 
     }
-    // Note : Si !_mute est faux et !_isDiagnostic est faux, 
-    // processedSample garde sa valeur 'sample' (Bypass micro direct).
-
-    // 4. Limitation et Conversion
+    
+    // 3. Limitation et Conversion
     processedSample = max(-1.0f, min(1.0f, processedSample));
     int16_t val = (int16_t)(processedSample * 32767.0f);
 
+    // 4. Envoi aux canaux
     outBlockL->data[i] = (earMode == 0 || earMode == 2) ? val : 0;
     outBlockR->data[i] = (earMode == 1 || earMode == 2) ? val : 0;
   }
