f25e39f406e483aab6a32b77d960dc3d3b875fc3
lilound <lilounadlerr@gmail.com>
2026-02-25 11:12:07 +0100

    Update MyDsp.h

--- main/MyDsp.h
+++ main/MyDsp.h
@@ -13,19 +13,23 @@ public:
   // Le constructeur doit être déclaré ici, mais défini dans le .cpp
   MyDsp();
   virtual void update(void);
+  float tick(float input);
   void setFreq(float f);
   void setFilter(int id, float gain, float centerFreq, float bandwidth);  
   void setEar(int mode); // pour gérer sur quelle oreille on envoie : canal 0 : le gauche, canal 1 : le droit, canal 2 : les 2
   void setMute(bool mute); // couper le son pour de vrai
   void setDiagnostic(bool diagnostic); // couper le son pour de vrai
+  void setAcouphene(bool actif, float freq, float age);
 
-  private:
+private:
   audio_block_t *inputQueueArray[1]; // Tableau pour stocker l'entrée
   bool _mute = false;
   bool _isDiagnostic = false; // Pour basculer entre source additive et micro
   Additive source; // Générateur de son
   PeakFilter filters[7]; // Tableau de 7 filtres
   int earMode;
+  Additive oscilloAcouphene; // Ton générateur Additive
+  bool acoupheneActif = false;
 
 };
 
